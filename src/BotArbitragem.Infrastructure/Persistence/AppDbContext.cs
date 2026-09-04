ff82b7e03a33e4677f61e55484ad90b84d07a49d
kevrauzika <166714341+kevrauzika@users.noreply.github.com>
2026-09-03 21:06:43 -0300

    feat: add PostgreSQL persistence foundation

---
+++ src/BotArbitragem.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,35 @@
+using BotArbitragem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BotArbitragem.Infrastructure.Persistence;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<FootballMatch> Matches => Set<FootballMatch>();
+    public DbSet<OddsQuote> OddsQuotes => Set<OddsQuote>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FootballMatch>(entity =>
+        {
+            entity.ToTable("matches");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.ExternalId).IsUnique();
+            entity.Property(x => x.ExternalId).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Competition).HasMaxLength(150).IsRequired();
+            entity.Property(x => x.HomeTeam).HasMaxLength(150).IsRequired();
+            entity.Property(x => x.AwayTeam).HasMaxLength(150).IsRequired();
+        });
+
+        modelBuilder.Entity<OddsQuote>(entity =>
+        {
+            entity.ToTable("odds_quotes");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Bookmaker).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Market).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Selection).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.DecimalOdds).HasPrecision(10, 4);
+            entity.HasOne<FootballMatch>().WithMany(x => x.OddsQuotes).HasForeignKey(x => x.MatchId);
+        });
+    }
+}
